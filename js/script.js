f7b24d30c885bbca8d13fa8ff206c2aa7f5c75d4
Jordan Ricca <jordan.ricca@lepoles.org>
2024-07-23 12:16:43 +0200

    javascript

---
+++ js/script.js
@@ -0,0 +1,74 @@
+// 1 Declaration d'une variable
+
+let temps;
+
+// 2 Assigner une valeur à la variable
+
+temps = "pluvieux";
+
+// Vérification de la valeur dans la console
+
+console.log(temps);
+
+alert("Super, tu es arrivé sur mon site !");
+window.alert("J'affiche une nouvelle boite de dialogue");
+
+// écrire sur la page
+document.write("<h1>C'est stylé le javascript !</h1>");
+
+// Demander à l'utilisateur une valeur (2 façons)
+let answer = prompt("Question: On est quel jour aujourd'hui ?");
+// window.prompt("Question: On est quel jour aujourd'hui ?")
+document.write(`<p>La réponse est :${answer}`);
+
+let weatherofday2;
+
+// CamelCase
+let weatherOfDay;
+
+// PascalCase
+let WeatherOfDay;
+
+// SnakeCase
+let weather_of_day;
+
+// Possibilité de réassigner une nouvelle valeur à notre variable de base
+temps = "nuageux";
+console.log(temps);
+
+
+/////////// LES TYPES DE VARIABLES/////////
+
+
+// 1 Chaine de caractères (string)
+let holidays = "2020";
+let destination = "Egypte";
+
+// 2 Un nombre entier (integer ou int)
+let year = 2015;
+
+// 3 un nombre décimal (float)
+let number = 3.55;
+
+// 4.Un booléen (boolean VRAI/FAUX = TRUE/FALSE)
+let condition = false;
+
+// 5.Une constante
+const PAYS = "France"; // string
+const YEAR = 2021; // integer
+
+// 6. connaitre le type d'une variable avec typeof
+console.log(typeof YEAR);
+console.log(typeof holidays);
+
+// NUMBER => STRING
+let otherNumber = 20;
+otherNumber = otherNumber.toString();
+console.log(typeof otherNumber);
+
+// STRING => NUMBER
+let number2 = "36";
+// let number2 = "36.5";
+number2 = parseInt(number2);
+// number2 = parseFloat(number2);
+console.log(typeof number2);
